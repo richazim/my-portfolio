f791a40f1b70034d265c9912f7940302985cf5bb
richazim <saibouazim90@gmail.com>
2025-02-25 17:21:10 +0000

    hide the first part of header on mobile screen

--- components/parts/Header.tsx
+++ components/parts/Header.tsx
@@ -6,7 +6,7 @@ import CustomLink from "@/components/CustomLink";
 const Header = () => {
   return (
     <div className="flex flex-row justify-between py-[20px] px-[30px]">
-      <CustomLink name="BOOK ME HERE" url="https://freelancer.com" className="font-bold border-[3px] py-[7px] px-[10px]"/>
+      <CustomLink name="BOOK ME HERE" url="https://freelancer.com" className="font-bold border-[3px] py-[7px] px-[10px] hidden sm:block"/>
       <ContactMenuBar/>
     </div>
   )
