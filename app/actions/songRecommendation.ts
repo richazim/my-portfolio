468302e65f3bdde36e46b20fdb376fcdc39b0bf1
richazim <saibouazim90@gmail.com>
2025-02-26 19:34:40 +0000

    Adding youtube api for song recommendations

--- app/actions/songRecommendation.ts
+++ app/actions/songRecommendation.ts
@@ -1,20 +1,23 @@
 "use server";
 
-export const getSongsRecommendation = async () => {
-    
+import {youtubeSongsRecommendtationsIds} from "@/app/constants";
 
-    return [
-        {
-            url: 'https://open.spotify.com/intl-fr/album/7Euqy9cTEqWGmJUn0pc5k8?si=aKx4Z8KNSiKC_4n6E7pfTQ'
-        },
-        {
-            url: 'https://open.spotify.com/intl-fr/album/7Euqy9cTEqWGmJUn0pc5k8?si=aKx4Z8KNSiKC_4n6E7pfTQ'
-        },
-        {
-            url: 'https://open.spotify.com/intl-fr/album/7Euqy9cTEqWGmJUn0pc5k8?si=aKx4Z8KNSiKC_4n6E7pfTQ'
-        },
-        {
-            url: 'https://open.spotify.com/intl-fr/album/7Euqy9cTEqWGmJUn0pc5k8?si=aKx4Z8KNSiKC_4n6E7pfTQ'
-        }
-    ]
+export const getSongsRecommendation = async () => {
+    const API_KEY = process.env.YOUTUBE_API_KEY;
+    const ids = youtubeSongsRecommendtationsIds.join(",");
+
+    const YOUTUBE_API_URL = `https://www.googleapis.com/youtube/v3/videos?id=${ids}&key=${API_KEY}&part=snippet,contentDetails,statistics`;
+
+    const res = await fetch(YOUTUBE_API_URL);
+    if (!res.ok) throw new Error("Erreur lors de la récupération des vidéos YouTube");
+
+    const data = await res.json();
+    console.log(data)
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    return data.items.map((video) => ({
+        id: video.id,
+        title: video.snippet.title,
+        thumbnail: video.snippet.thumbnails.high.url,
+    }))
 }
--- components/parts/SongsRecommendation.tsx
+++ components/parts/SongsRecommendation.tsx
@@ -9,10 +9,11 @@ const SongsRecommendation = async () => {
     return (
         <div className="flex flex-col justify-between w-[100vw] overflow-hidden">
             <section className="flex flex-row justify-between gap-[20px] items-center h-[300px]">
-                {SONGS_RECOMMENDATIONS.map((song, index) => (
-                    <div key={index} className="w-[250px] flex items-center justify-center border-[1px]">
-                        <Link href={song.url} className="h-[200px] w-[200px]">
-                            <Image src="/assets/images/profile.png" alt="" width={200} height={200}/>
+                {/* @ts-expect-error: L'API Youtube n'a pas défini de type pour les reponses api. */}
+                {SONGS_RECOMMENDATIONS.map((song, index: number) => (
+                    <div key={index} className="flex items-center justify-center">
+                        <Link href={`https://youtube.com/watch?v=${song.id}`} target="_blank" className="h-[200px] w-[350px] flex justify-center items-center overflow-hidden">
+                            <Image src={song.thumbnail} alt="" width={480} height={360} className=""/>
                         </Link>
                     </div>
                 ))}
@@ -20,7 +21,7 @@ const SongsRecommendation = async () => {
 
             <div className='mt-[50px] flex justify-center items-center'>
                 <p className="text-center font-bold w-[400px]">
-                    A FEW SONG I CAN RECOMMEND IF YOU'RE LOOKING FOR SOME GOOD ONES
+                    A FEW SONG I CAN RECOMMEND IF YOU&apos;RE LOOKING FOR SOME GOOD ONES
                 </p>
             </div>
         </div>
