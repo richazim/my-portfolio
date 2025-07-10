b4eac4de6c4a748e668da3d99cfc73a5effb3b12
richazim <saibouazim90@gmail.com>
2025-07-10 02:02:16 +0100

    React Animated Cursor

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import Preloader from "@/components/Preloader";
 import RootPage from "@/pages/RootPage"
+import AnimatedCursor from "react-animated-cursor";
 
 const Page = () => {
 
@@ -7,6 +8,27 @@ const Page = () => {
     <>
       <Preloader/>
       <RootPage/>
+      <AnimatedCursor 
+        innerSize={15}
+        outerSize={15}
+        color='251, 191, 48'
+        outerAlpha={0.2}
+        innerScale={1.7}
+        outerScale={5}
+        clickables={[
+          'a',
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          'label[for]',
+          'select',
+          'textarea',
+          'button',
+          '.link'
+        ]}
+      />
     </>
   )
 }
