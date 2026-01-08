9a3b8d0d790cd4b76d5b888189c6625ff91147c9
richazim <saibouazim90@gmail.com>
2026-01-08 04:22:02 +0100

    refactor:project structure refactored

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,11 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./sections/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
